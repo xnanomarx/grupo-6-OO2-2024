0db641efbf5132296d15fe4ffc65d5a4de465787
xnanomarx <marianoramirez0714@gmail.com>
2024-06-11 22:50:05 -0300

    Primer commit

---
+++ grupo3/src/main/java/com/unla/grupo3/helpers/ViewRouteHelper.java
@@ -0,0 +1,16 @@
+package com.unla.grupo3.helpers;
+
+public class ViewRouteHelper {
+	/**** Views ****/
+	//HOME
+	public final static String INDEX = "home/index";
+	public final static String HELLO = "home/hello";
+	
+	//USER
+	public final static String USER_LOGIN = "user/login";
+	public final static String USER_LOGOUT = "user/logout";
+	
+	/**** Redirects ****/
+	public final static String ROUTE = "/index";
+	
+}
---
+++ grupo3/src/test/java/com/unla/grupo22/Grupo22ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unla.grupo22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo22ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
