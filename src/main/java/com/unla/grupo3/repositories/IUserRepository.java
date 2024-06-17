30b6511ddf6a2eb7ec5788336703d214b00f751d
xnanomarx <marianoramirez0714@gmail.com>
2024-06-17 20:47:59 -0300

    logout

--- src/main/java/com/unla/grupo3/repositories/IUserRepository.java
+++ src/main/java/com/unla/grupo3/repositories/IUserRepository.java
@@ -2,10 +2,12 @@ package com.unla.grupo3.repositories;
 
 import com.unla.grupo3.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IUserRepository extends JpaRepository<User, Long> {
-    @Query("SELECT u FROM User u LEFT JOIN FETCH u.userRoles WHERE u.username = :username")
-    User findByUsernameAndFetchUserRolesEagerly(@Param("username") String username);
+
+    User findByUsername(String username);
+
+    User findByUsernameAndFetchUserRolesEagerly(String username);
 }
