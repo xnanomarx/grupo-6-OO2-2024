9aa0905fb567d0c81aad2214067d3d4830b3611c
xnanomarx <marianoramirez0714@gmail.com>
2024-06-18 15:03:59 -0300

    1

--- src/main/java/com/unla/grupo3/repositories/IUserRepository.java
+++ src/main/java/com/unla/grupo3/repositories/IUserRepository.java
@@ -1,7 +1,10 @@
 package com.unla.grupo3.repositories;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.unla.grupo3.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -9,5 +12,6 @@ public interface IUserRepository extends JpaRepository<User, Long> {
 
     User findByUsername(String username);
 
-    User findByUsernameAndFetchUserRolesEagerly(String username);
+    @Query(value="select * from baseprueba.user u where u.username = :username", nativeQuery=true)
+    User findByUsernameAndFetchUserRolesEagerly(@Param("username") String username);
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,1 @@
+INSERT INTO user_role (created_at, role, updated_at, user_id) VALUES ('2024-06-09', 'ROLE_USER', '2024-06-09', 7);
