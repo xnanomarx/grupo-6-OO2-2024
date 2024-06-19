c5552bba0cd16f717c74844959ed882f48da446a
Martin3606 <martinvaz.riv@hotmail.com>
2024-06-18 22:47:31 -0300

    apertura por primera vez
    Se agregó la clase "DataInitializer" en la que cuando se inicia el programa por primera vez, se agrega automáticamente un usuario "admin" de contraseña "1234"

--- src/main/java/com/unla/grupo3/DataInitializer.java
+++ src/main/java/com/unla/grupo3/DataInitializer.java
@@ -1,8 +1,10 @@
-/*package com.unla.grupo3;
+package com.unla.grupo3;
 
 import com.unla.grupo3.entities.User;
 import com.unla.grupo3.entities.UserRole;
 import com.unla.grupo3.repositories.IUserRepository;
+import com.unla.grupo3.repositories.IUserRoleRepository;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
@@ -14,17 +16,21 @@ import java.util.Set;
 public class DataInitializer {
 
     private final IUserRepository userRepository;
+    private final IUserRoleRepository userRoleRepository;
     private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public DataInitializer(IUserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public DataInitializer(IUserRepository userRepository, IUserRoleRepository userRoleRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.userRoleRepository = userRoleRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
+    @PostConstruct
     public void initialize() {
         // Verificar si ya existe un usuario con el nombre "admin"
             // Crear un nuevo usuario administrador
+        if (userRepository.findByUsername("admin") == null) {
             User admin = new User();
             admin.setUsername("admin");
             admin.setPassword(passwordEncoder.encode("1234")); // Codificar la contraseña
@@ -40,8 +46,11 @@ public class DataInitializer {
             roles.add(adminRole);
             admin.setUserRoles(roles);
 
+
             // Guardar el usuario en la base de datos
             userRepository.save(admin);
+            userRoleRepository.save(adminRole);
+        }
 
     }
-}*/
+}
